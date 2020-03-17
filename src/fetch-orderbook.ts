08afb9ccaed72ca3a135ab5ea6275333962ac69a
Kam, Dongki <dkkahm@gmail.com>
2020-03-17 09:21:25 +0900

    currency list

--- src/fetch-orderbook.ts
+++ src/fetch-orderbook.ts
@@ -4,9 +4,15 @@ import * as mongodb from "mongodb";
 import { OrderBook, OrderBookModel } from "./models/OrderBook";
 const mongoose = require("mongoose");
 
+enum Currency {
+  XRP = "XRP",
+  BTC = "BTC"
+}
+
 const db_url = "mongodb://localhost/fetch-orderbook";
-const fetch_orderbook_url = "https://api.bithumb.com/public/orderbook/XRP";
+const fetch_orderbook_url_base = "https://api.bithumb.com/public/orderbook/";
 const INTERVAL_MS = 5000;
+const target_currcy_list = [Currency.XRP, Currency.BTC];
 
 function sleep(ms: number): Promise<void> {
   return new Promise((resolve, reject) => {
@@ -16,8 +22,8 @@ function sleep(ms: number): Promise<void> {
   });
 }
 
-async function fetchOrderbook() {
-  const result = await axios.get(fetch_orderbook_url);
+async function fetchOrderbook(currency: Currency) {
+  const result = await axios.get(fetch_orderbook_url_base + currency);
   // console.log(result.data);
   return result.data;
 }
@@ -36,10 +42,12 @@ async function main() {
   while (true) {
     await sleep(INTERVAL_MS);
 
-    const data = await fetchOrderbook();
-    if (data.status === "0000") {
-      await saveOrderbook(data.data);
-    }
+    target_currcy_list.forEach(async currency => {
+      const data = await fetchOrderbook(currency);
+      if (data.status === "0000") {
+        await saveOrderbook(data.data);
+      }
+    });
   }
 }
 
